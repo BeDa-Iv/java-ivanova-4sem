42ea7bce4476dcf9a41e6137638135d1c5ec2a66
Дарья <dashana2112@gmail.com>
2021-05-21 16:16:14 +0300

    Переделки

--- src/javafxexamples/ListenersExamples.java
+++ src/javafxexamples/ListenersExamples.java
@@ -7,11 +7,9 @@ import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
